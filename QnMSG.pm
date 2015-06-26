7af3120a5573a4cfd676124083e81aed484a51ae
vitduck <levietduc@kaist.ac.kr>
2015-06-26 21:09:17 +0900

    Add get_capacity to get size of partition

--- QnMSG.pm
+++ QnMSG.pm
@@ -8,18 +8,20 @@ use File::Basename;
 use List::Util qw(sum); 
 
 # symbol 
-our @user   = qw( get_users ); 
-our @hdd    = qw( get_hdd print_hdd ); 
-our @zombie = qw( print_status zombie_sweep ); 
+our @user   = qw( get_user ); 
+our @hdd    = qw( get_partition get_disk_usage print_disk_usage ); 
+our @zombie = qw( zombie_sweep ); 
+our @status = qw( print_status ); 
 
 # default import (all) 
-our @EXPORT = ( @user, @hdd, @zombie ); 
+our @EXPORT = ( @user, @hdd, @zombie, @status ); 
 
 # tag import 
 our %EXPORT_TAGS = ( 
     user   => \@user,  
     hdd    => \@hdd, 
     zombie => \@zombie, 
+    status => \@status, 
 ); 
 
 ########
@@ -31,14 +33,14 @@ our %EXPORT_TAGS = (
 #   - none 
 # return: 
 #   - hash of user (home => { user => homedir })
-sub get_users { 
+sub get_user { 
     my %passwd; 
 
     open my $passwd, '<', '/etc/passwd' or die "Cannot open /etc/passwd\n"; 
     while ( <$passwd> ) { 
         if ( /\/home2?\// ) { 
             my ($user, $homedir) = (split ':')[0,5]; 
-            my $home = (fileparse($homedir))[1]; 
+            my $home = dirname($homedir); 
             $passwd{$home}{$user} = $homedir; 
         }
     }
@@ -51,31 +53,69 @@ sub get_users {
 # HDD #
 #######
 
+# get partition
+# arg: 
+#   - none 
+# return: 
+#   - hash ( partition => size )
+sub get_partition { 
+    # pipe to df 
+    open DF, '-|', 'df -B G' or die "Cannot open pipe to df\n"; 
+    my @output = <DF>; 
+    close DF; 
+    
+    # remove output header 
+    shift @output; 
+    my %df = map { (split)[-1,1] } @output; 
+
+    # strip the suffix G 
+    my @partitions = sort keys %df; 
+    @df{@partitions} = map { $1 if $df{$_} =~ /(\d+)G/ } @partitions; 
+
+    return %df;  
+}
+
 # get users' disk usage 
 # arg: 
 #   - hash of user
 # return: 
 #   - hash of disk usage (home => { user => usage })
-sub get_hdd { 
+sub get_disk_usage { 
     my %passwd = @_; 
-    my %hdd; 
+    my %du; 
     
+    my $count  = 0; 
+    my $column = 4; 
+
     for my $home ( keys %passwd ) { 
         # ref to hash of users in $home
         my $r2user = $passwd{$home}; 
+
+        # list users in $home partition
+        my @users = keys %$r2user; 
+
+        # string format for status line 
+        my $slength = (sort {$b <=> $a} map length($_), @users)[0]; 
+
         # loop through all ids and collect disk usage 
-        for my $user ( keys %$r2user ) { 
+        for my $user ( @users ) { 
             my $homedir = $r2user->{$user}; 
             # only physical user 
             next unless -d $homedir; 
 
+            # print status line 
+            print_status(++$count, $column, $user, $slength); 
+            
+            # line break for last user
+            if ( $user eq $users[-1] && $count % $column != 0 ) { print "\n" };
+
             # capture du output with backtick
             my ($usage) = (split ' ', `du -sBG $homedir`)[0]; 
-            $hdd{$home}{$user} = $usage; 
+            $du{$home}{$user} = $usage; 
         }
     }
 
-    return %hdd;  
+    return %du; 
 }
 
 # print users' disk usage 
@@ -84,11 +124,11 @@ sub get_hdd {
 #   - disk usage cut-off 
 # return: 
 #   - null
-sub print_hdd { 
-    my ($r2hdd, $cutoff) = @_; 
+sub print_disk_usage { 
+    my ($r2hdd, $capacity, $cutoff) = @_; 
 
     # list of users in $home 
-    my @users      = sort keys %$r2hdd;
+    my @users = sort keys %$r2hdd;
 
     # strip the 'G' suffix, and slice the hash ref
     @{$r2hdd}{@users} = map { $1 if $r2hdd->{$_} =~ /(\d+)G/ } @users; 
@@ -96,24 +136,28 @@ sub print_hdd {
     # total disk usage 
     my $total   = sum(@{$r2hdd}{@users}); 
 
-    # output format for string and digit
+    # string & digit format for table 
     my $slength = (sort {$b <=> $a} map length($_), @users)[0]; 
     my $dlength = (sort {$b <=> $a} map length($_), @{$r2hdd}{@users})[0]; 
     
     # summation of total usage 
     # this is used to draw dynamic dash line 
-    my $summary = sprintf "%${slength}s  %${dlength}d GB  %6.2f %%", 'total', $total, 100; 
+    my $summary = sprintf "%${slength}s  %${dlength}d GB  %6.2f %%", 'total', $total, 100*$total/$capacity; 
 
-    # print table
-    print "-" x length($summary); 
-    print "\n"; 
+    # print -----
+    print "-" x length($summary); print "\n"; 
+
+    # table description: user -> usage -> % usage [*]
     for my $user ( sort { $r2hdd->{$b} <=> $r2hdd->{$a} } keys %$r2hdd ) { 
-        printf "%${slength}s  %${dlength}d GB  %6.2f %%", $user, $r2hdd->{$user}, 100*$r2hdd->{$user}/$total; 
+        printf "%${slength}s  %${dlength}d GB  %6.2f %%", $user, $r2hdd->{$user}, 100*$r2hdd->{$user}/$capacity; 
+        # print a [*] if a user uses more than cut-off 
         $r2hdd->{$user} >= $cutoff ? print " [*]\n" : print "\n"; 
     }
+
+    # print -----
     print "-" x length($summary); 
     
-    # print summary at the end 
+    # print summary (total usaga % usage)
     print "\n$summary\n"; 
 
     return 0; 
@@ -122,28 +166,6 @@ sub print_hdd {
 ##########
 # ZOMBIE #
 ##########
-
-# print status line during scan
-# args: 
-#   - current node count 
-#   - node_id (x0??)
-#   - node_status (down* ?)
-# return: 
-#   - null
-sub print_status { 
-    my ($count, $node_id, $node_status) = @_; 
-    
-    # mark down node and return immediately 
-    if ( $node_status =~ /down\*/ ) { 
-        printf "->%5s ", "down"; 
-        return 1;  
-    } else { 
-        # 8 nodes per line 
-        $count % 8 == 0 ? printf "->%5s\n", $node_id : printf "->%5s ", $node_id; 
-    }
-
-    return 0;  
-}
 
 # rsh and parse the output of ps for user processes 
 # args: 
@@ -197,3 +219,34 @@ sub zombie_sweep {
     
     return 0;  
 }
+
+##########
+# STATUS #
+##########
+
+# print status line during scan
+# args: 
+#   - current node count 
+#   - node_id (x0??|user)
+#   - node_status (down* ?)
+# return: 
+#   - null
+sub print_status { 
+    my ($count, $column, $process, $slength) = @_; 
+
+    # addition status (down* or null)
+    my $status = shift @_ || ''; 
+    
+    # mark down node and return immediately 
+    if ( $status =~ /down\*/ ) { 
+        printf "->% ${slength}s ", "down"; 
+        return 1;  
+    } else { 
+        # generic counting status
+        $count % $column == 0 ? printf "-> %${slength}s\n", $process : printf "-> %${slength}s ", $process; 
+    }
+
+    return 0;  
+}
+
+1; 
