9979388b0be6010d6b319efae265bccdb8d9dbd7
vitduck <levietduc@kaist.ac.kr>
2015-06-26 12:42:16 +0900

    Add subroutine to get users' disk usage

--- QnMSG.pm
+++ QnMSG.pm
@@ -5,15 +5,38 @@ use warnings;
 use Exporter qw(import); 
 
 # symbol 
-our @EXPORT = qw( get_users ); 
+our @EXPORT = qw( get_users get_hdd ); 
 
-#########
-# USERS #
-#########
+# get hash of user 
+# arg : 
+#   - none 
+# return: 
+#   - hash of user ( user => homedir )
 sub get_users { 
     open my $passwd, '<', '/etc/passwd' or die "Cannot open /etc/passwd\n"; 
     my %users = map { (split ':')[0,5] } grep /\/home2?\//, <$passwd>; 
     close $passwd; 
     
     return %users;  
+}
+
+# get users' disk usage 
+# arg: 
+#   - hash of user
+# return: 
+#   - hash of disk usage ( user => disk )
+sub get_hdd { 
+    my %passwd = @_; 
+    my %hdd; 
+    for my $user ( sort keys %passwd ) { 
+        my $homedir = $passwd{$user}; 
+        # only physical user 
+        next unless -d $homedir; 
+
+        # capture du output with backtick
+        my ($usage) = (split ' ', `du -sBG $homedir`)[0]; 
+        $hdd{$user} = $usage; 
+    }
+
+    return %hdd;  
 }
